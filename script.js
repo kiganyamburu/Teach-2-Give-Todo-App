bed15ff4e2455a04700d73c91482c610991ce773
kiganyamburu <mburukiganya@gmail.com>
2024-05-16 10:54:05 +0300

    adding css and js file

--- script.js
+++ script.js
@@ -1,100 +1,116 @@
-const theme = document.getElementById('theme');
-const newItemInput = document.getElementById('addItem');
-const todoList = document.querySelector('.content ul');
-const itemsLeft = document.querySelector('item-left span');
-
-itemsLeft.innerText = document.querySelectorAll('.list-item input[type="check"')
-
-theme.addEventListener('click', () =>){
-    document.querySelector('body').classList = [theme.checked ? 'theme-light' : 'theme]
-});
-
-document.querySelector('.add-new-item span').addEventListener('click', () =>)
-if (newItemInput.value.length > 0) {
-    createNewTodoItem(newItemInput.value);
-    newItemInput.value = '';
-
-}
-});
-
-newItemInput.addEventListener('keypress', (e) => {
-    if (e.charCode == 13 && newItemInput.value.length > 0) {
-        createNewTodoItem(newItemInput.value);
-        newItemInput.value = '';
-    }
-});
-
-function createNewTodoItem(text) {
-    const elem = document.createElement('li');
-    elem.classList.add('flex-row');
-
-    elem.innerHTML = 
-        <label class="list-item">
-            <span class="checkmark"></span>
-            <span class="text">${text}</span>
-        </label>
-    <span class="remove"></span>
-        ;
-    
-    if (document.querySelector('.filter input[type="radio"]: checked').id ==)
-        elem.classList.add('hidden');
-}
-
-todoList.append(elem);
-updateItemsCount(1);
-}
-
-function updateItemsCount(number) {
-    itemsLeft.innerText = +itemsLeft.innerText + number;
-}
-
-// remove todo item
-
-
-function removeTOdoItem(elen) {
-    elen.remove();
-    updateItemsCount(-1);
-}
-
-todoList.addEventListener('click', (event) => {
-    if (event.target.classList.contains('remove')) {
-        removeTOdoItem(event.target.parentElement);
-    }
-});
-
-// clear completed items
-
-document.querySelector('.clear').addEventListener('click', () => {
-    document.querySelectorAll('.list-item input[type="checkbox"]:check').forEach
-         removeTOdoItem(item.closest('li'));
-}   );
-});
-
-// filter todo list items
-document.querySelectorAll('.filter input').forEach(radio => {
-    radio.addEventListener('change', (e) => {
-        filterTodoItems(e.target.id);
-    });
-});
-
-function filterTodoItems(id) {
-    const allItems = todoList.querySelectorAll('li');
-
-    switch (id) {
-        case 'all':
-            allItems.forEach(item => {
-                item.classList.remove('hidden');
-            })
-            break;
-        case 'active':
-            allItems.forEach(item => {
-                item.querySelector('input').checked ? item.classList.add('hidden')
-            })
-            break;
-            allItems.forEach(item => {
-                !item.querySelector('input').checked ? item.classList.add('hidden') 
-            })
-break 
-            }
-    }
-}
+var app = new Vue({
+    el: "#app",
+    data: {
+      list: [
+        { checked: true, text: "Complete online JavaScript course", id: 1 },
+        { checked: false, text: "Jog around the park 3x", id: 2 },
+        { checked: false, text: "10 minutes meditation", id: 3 },
+        { checked: false, text: "Read for 1 hour", id: 4 },
+        { checked: false, text: "Pick up groceries", id: 5 },
+        { checked: false, text: "Complete Todo App on Frontend Mentor", id: 6 },
+      ],
+      optionActive: "All",
+      options: [{ name: "All" }, { name: "Active" }, { name: "Completed" }],
+      filteredList: null,
+      createTodoInput: "",
+      itemLeft: null,
+      lastSelection: null,
+      darkTheme: false,
+    },
+    methods: {
+      createItem(text) {
+        let item = new Object();
+        item.checked = false;
+        item.text = `${text}`;
+        return item;
+      },
+      push() {
+        if (this.createTodoInput === "") {
+          return;
+        }
+        this.list.push(this.createItem(this.createTodoInput));
+        this.filterList(this.lastSelection);
+        this.createTodoInput = "";
+      },
+      check(index) {
+        this.list.map((item) => {
+          if (item.id === index) {
+            item.checked = true;
+          }
+        });
+        this.filterList(this.lastSelection);
+      },
+      clearChecked() {
+        let indexToDelete = this.list.filter((item) => item.checked === true);
+        indexToDelete.forEach((item) => {
+          this.list.splice(this.list.indexOf(item), 1);
+        });
+        this.filterList(this.lastSelection);
+      },
+      deleteTodo(index) {
+        let indexToDelete;
+        this.list.map((item) => {
+          if (item.id === index) {
+            indexToDelete = this.list.indexOf(item);
+          }
+        });
+        this.list.splice(indexToDelete, 1);
+        this.filterList(this.lastSelection);
+      },
+      filterList(val) {
+        this.optionActive = val;
+        console.log(val);
+        let dic = {
+          All: null,
+          Active: false,
+          Completed: true,
+        };
+        value = dic[val];
+  
+        if (value === null) {
+          this.filteredList = this.list.filter((item) => {
+            return item.checked != null;
+          });
+        } else {
+          this.filteredList = this.list.filter((item) => {
+            return item.checked === value;
+          });
+        }
+        this.lastSelection = val;
+      },
+      themeSwitcher() {
+        this.darkTheme = !this.darkTheme;
+        if (this.darkTheme) {
+          document.body.classList.add("dark");
+          document.body.classList.remove("light");
+        } else {
+          document.body.classList.remove("dark");
+          document.body.classList.add("light");
+        }
+      },
+    },
+    computed: {
+      detectItemLeft() {
+        let i = 0;
+        this.list.forEach((item) => {
+          if (item.checked != true) {
+            i++;
+          }
+        });
+        return i;
+      },
+    },
+    created: function () {
+      this.filterList("All");
+      this.themeSwitcher();
+    },
+    mounted: function () {
+      let draggable = document.querySelector(".todo__list");
+  
+      new Sortable(draggable, {
+        animation: 150,
+        ghostClass: "blue-background-class",
+      });
+    },
+  });
+  
